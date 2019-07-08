9df8ef7eb9763582031777bd295f63699ec386ac
katarinadlukic <40492319+katarinadlukic@users.noreply.github.com>
2019-07-08 19:38:07 +0200

    konstrutor, kreiranje kontakta i ispis

--- imenik/main.js
+++ imenik/main.js
@@ -1,21 +1,33 @@
-let name = document.getElementById("name");
-let lastName = document.getElementById("lastName");
-let address = document.getElementById("address");
-let mobileNum = document.getElementById("mobileNum");
-let homeNum = document.getElementById("homeNum");
 let names = document.getElementById("section2");
+let contacts = [];
+function Contact(name, lastName, address, mobileNum, homeNum){ // konstruktor
+    this.name  = name;
+    this.lastName = lastName;
+    this.address = address;
+    this.mobileNum = mobileNum;
+    this.homeNum = homeNum;
+}
 
-function addContact(){
-let nameVal = name.value;
-let lastNameVal = lastName.value;
-let addressVal = address.value;
-let mobile = mobileNum.value;
-let home = homeNum.value;
- names.innerHTML += `
- <p>${nameVal}</p>
- <p>${lastNameVal}</p>
- <p>${addressVal}</p>   
- <p>${mobile}</p>
- <p>${home}</p>
- `
+function createContact(){ // kreiranje kontakta i upisivanje u array
+    let nameVal = document.getElementById("name").value;
+    let lastNameVal = document.getElementById("lastName").value;
+    let addressVal = document.getElementById("address").value;
+    let mobile = document.getElementById("mobileNum").value;
+    let home = document.getElementById("homeNum").value;
+    let contact = new Contact(nameVal, lastNameVal, addressVal, mobile, home);
+
+    contacts.push(contact);
+    addContact(contacts);
+}
+
+function addContact(contacts){ //ispis podataka
+        let x  = contacts[contacts.length-1];
+        names.innerHTML += `
+        <p><span>Ime i prezime:</span> ${x.name} ${x.lastName}</p>
+        <p><span>Adresa:</span> ${x.address}</p>
+        <p><span>Mobilni:</span> ${x.mobileNum}</p>
+        <p><span>Fiksni:</span> ${x.homeNum}</p>
+        <hr>
+        `
+        
 }
