80b94661d03ae8b702466b07448e965dbe473bb5
katarinadlukic <40492319+katarinadlukic@users.noreply.github.com>
2019-07-08 05:25:03 +0200

    linkovanje reci koje pocinju sa @

---
+++ twitter_parser/main.js
@@ -0,0 +1,17 @@
+let inputField = document.getElementById("inputField");
+let res = document.getElementById("res");
+
+// rec koja pocinje sa @ postaje link, i uklanja se char @
+function linkIt(){
+    let str = inputField.value;
+    let arr = [];
+    arr = str.split(" ");
+    for(let i=0; i < arr.length; i++){
+        if (arr[i].includes("@")){
+            let x = arr[i].replace(/@/g, "");
+            arr[i] = `<a href="https://twitter.com/?lang=en">${x}</a>`
+        }
+    }
+    let parsed = arr.join(" ");
+    res.innerHTML = parsed;
+}
