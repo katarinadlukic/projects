0fc8cc3272436a0002c166e217a29676d88f8190
katarinadlukic <40492319+katarinadlukic@users.noreply.github.com>
2019-07-09 03:16:25 +0200

    search funkcija

--- imenik/main.js
+++ imenik/main.js
@@ -1,5 +1,6 @@
 let names = document.getElementById("section2");
-let contacts = [];
+let form = document.getElementById("section1");
+let contacts = [];// niz u kome ce se cuvati objekti sa informacijama o kontaktu
 
 function Contact(name, lastName, address, mobileNum, homeNum){ // konstruktor
     this.name  = name;
@@ -30,18 +31,38 @@ function createContact(){ // kreiranje kontakta i upisivanje u array
         }
         return 0;
  }
+ function removeValues(){ // funkcija za brisanje input polja
+     console.log(form.children.length);
+     for(let i=0; i < form.children.length; i++){
+         form.children[i].value = "";
+     }
+ }
 function addContact(contacts){ //ispis podataka
-        names.innerHTML = "";
+        removeValues();// brisanje vrednosti iz input polja
+        names.innerHTML = "";// brisanje starog ispisa liste
         let x  = contacts.sort(compare); // sortiranje niza po abecednom redu na osnovu imena korisnika
         for (let i = 0; i < x.length;i++){
             names.innerHTML += `
-        <li><span>Ime i prezime:</span> ${x[i].name} ${x[i].lastName}</ul>
-        <li><span>Adresa:</span> ${x[i].address}</ul>
-        <li><span>Mobilni:</span> ${x[i].mobileNum}</ul>
-        <li><span>Fiksni:</span> ${x[i].homeNum}</ul>
+        <li>${x[i].name} ${x[i].lastName}<br>
+        ${x[i].address}<br>
+        ${x[i].mobileNum}<br>
+        ${x[i].homeNum}<br>
+        </ul>
         <br><br>
         `
         }
-        
-        
-}
+    }  
+function filterSearch() { // funkcija koja pretrazuje karaktere u listi, filter
+    let input = document.getElementById("filterInput");
+    let filter = input.value.toUpperCase();
+    let ul = document.getElementById("section2");
+    let li = ul.getElementsByTagName("li");
+    for (let i = 0; i < li.length; i++) {
+        if (li[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+ }
+
