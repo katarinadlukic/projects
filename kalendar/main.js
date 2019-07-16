c8da7ccf4155bef29eaafe93ee9129db52ad7b88
katarinadlukic <40492319+katarinadlukic@users.noreply.github.com>
2019-07-16 12:26:54 +0200

    date, clock

--- kalendar/main.js
+++ kalendar/main.js
@@ -1,16 +1,21 @@
 let calendar = document.getElementById("calendar");
 let weekDays = document.getElementById("daysOfTheWeek");
 let dayWeekNum = [7, 1, 2, 3, 4, 5, 6];
-let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+let days = ["Sun","Mon", "Tue", "Wed", "Thu", "Fri", "Sat" ];
 let months = ["January", "February", "March", "April", "May" , "June" ,"July", "August", "September", "October", "November", "December"];
 let yearElement = document.getElementById("year");
 let monthElement = document.getElementById("month");
-let currentMonth = 6;
-// new Date().getMonth();
+let currentMonth = new Date().getMonth();
 let currentYear = new Date().getFullYear();
-let currentDay = new Date().getDate();
+let currentDay = new Date().getDate();// dan u mesecu
 let months31 = [0,2,4,6,7,9,11];
 let months30 = [3,5,8,10];
+let previousBtn = document.getElementById("previous");
+let nextBtn = document.getElementById("next");
+previousBtn.addEventListener("click", previous);
+nextBtn.addEventListener("click", next );
+let livetime = document.getElementById("time");
+
 
 function createThead(){
     for(let i = 0; i < days.length; i++){
@@ -21,7 +26,7 @@ function createThead(){
     }
 }
 function yearAndMonth(){
-    yearElement.textContent = `${currentYear}`;  
+    yearElement.textContent = `${currentDay}/${months[currentMonth]}/${currentYear}`; 
     monthElement.textContent = `${months[currentMonth]}`; 
 }
 function createTr(){
@@ -69,38 +74,60 @@ function createTr(){
 
     }
 }
-function createData(){
-    let start = dayWeekNum[new Date(currentYear,currentMonth,1 ).getDay()]  ; //0
+
+function createData(thisMonth){
+    let start = dayWeekNum[new Date(currentYear,thisMonth,1 ).getDay()]  ; //0
     let counter = 1;
     let numberOfDays;
     for (let i = 0; i < months31.length; i++){
-     if(currentMonth == months31[i]){
+     if(thisMonth == months31[i]){
         numberOfDays = 31;
         break;
      }
     }
     for (let i = 0; i < months30.length; i++){
-        if(currentMonth == months30[i]){
+        if(thisMonth == months30[i]){
             numberOfDays = 30;
            break;
         }
        }
-    if(currentMonth == 1){
+    if(thisMonth == 1){
         numberOfDays = 28;
     }
-    console.log(numberOfDays)
-    //let x = 1;
     for (let i = start; i < numberOfDays + start ; i++){
             document.getElementById(`td${i}`).textContent = `${counter}`;
             if (currentDay == counter){
-                document.getElementById(`td${i}`).style.backgroundColor = "red";
+                document.getElementById(`td${i}`).style.backgroundColor = " #f03333";
             }
             counter++; 
     }
 }
+function time() {
+    livetime.innerHTML = "";
+    var d = new Date();
+    var s = d.getSeconds();
+    if (s < 10){
+        s = "0" + s;
+    }
+    var m = d.getMinutes();
+    if (m < 10){
+        m = "0" + m;
+    }
+    var h = d.getHours();
+    if (h < 10){
+        h = "0" + h;
+    }
+    livetime.innerHTML = h + ":" + m + ":" + s;
+  }
+  
+setInterval(time, 1000);
+
 yearAndMonth();
 createThead();
 createTr();
-createData();
+createData(currentMonth);
+
+
+
 
 
