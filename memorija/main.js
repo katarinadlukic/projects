13296f46c72a773e0ab23fa49928ac46319de510
katarinadlukic <40492319+katarinadlukic@users.noreply.github.com>
2019-07-18 08:50:02 +0200

    adding images, creating json

--- memorija/main.js
+++ memorija/main.js
@@ -3,119 +3,169 @@ let btnStart = document.getElementById("start");
 let container = document.getElementById("container");
 let scoreBoard = document.getElementById("scoreBoard");
 let gameDiv = document.getElementById("gameDiv");
-let tbl = document.getElementById("table"); 
-    function enterGame() {
-            // let x = readCookie("username");
-            // if(x){
-            //     createElements(x);
-            // }else {
-            //     let person = prompt("Please enter your name");
-            //     createCookie("username",person, 1);
-            // }
-            let x = "Katarina"
-            createElements(x);
-  }
-  function listeners(){
-      tryIt.addEventListener("click", enterGame);
-       start.addEventListener("click", gameStart);
-  }
- 
+let tbl = document.getElementById("table");
 
-  function createElements(person){
+function enterGame() {
+    // let x = readCookie("username");
+    // if(x){
+    //     createElements(x);
+    // }else {
+    //     let person = prompt("Please enter your name");
+    //     createCookie("username",person, 1);
+    // }
+    let x = "Katarina"
+    createElements(x);
+}
+
+function listeners() {
+    tryIt.addEventListener("click", enterGame);
+    start.addEventListener("click", gameStart);
+}
+
+
+function createElements(person) {
     tryIt.style.display = "none"; // brisanje tryIt dugmeta sa stranice
     container.style.visibility = "visible";
     let welcomeMsg = document.getElementById("welcome");
-     welcomeMsg.textContent = `Welcome ${person}!`;
-     generateTable()
- 
-  }
+    welcomeMsg.textContent = `Welcome ${person}!`;
+    generateTable()
 
- 
-  function createCookie(name,value,days) {
-	if (days) {
-		var date = new Date();
-		date.setTime(date.getTime()+(days*24*60*60*1000));
-		var expires = "; expires="+date.toGMTString();
-	}
-	else var expires = "";
-	document.cookie = name+"="+value+expires+"; path=/";
+}
+
+
+function createCookie(name, value, days) {
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        var expires = "; expires=" + date.toGMTString();
+    } else var expires = "";
+    document.cookie = name + "=" + value + expires + "; path=/";
 }
 
 function readCookie(name) {
-	var nameEQ = name + "=";
-	var ca = document.cookie.split(';');
-	for(var i=0;i < ca.length;i++) {
-		var c = ca[i];
-		while (c.charAt(0)==' ') c = c.substring(1,c.length);
-		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-	}
-	return null;
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
 }
 
 function eraseCookie(name) {
-	createCookie(name,"",-1)
+    createCookie(name, "", -1)
 }
 
-function lvlOptions(){
+function lvlOptions() {
     let lvlTxt = document.getElementById("lvlTxt");
     lvlTxt.textContent = " You have 60 sec to finish the game";
     let a = document.getElementById('level');
     a.addEventListener('change', function() {
-       if (this.value == 0){
-        lvlTxt.textContent = " You have 60 sec to finish the game";
-    }
-        if (this.value == 1){
+        if (this.value == 0) {
+            lvlTxt.textContent = " You have 60 sec to finish the game";
+        }
+        if (this.value == 1) {
             lvlTxt.textContent = " You have 30 sec to finish the game";
         }
-        if (this.value == 2){
+        if (this.value == 2) {
             lvlTxt.textContent = " You have 15 sec to finish the game";
         }
     })
 }
 
-function generateTable(){
-    
+function generateTable() {
+    let idArr = shuffle();
+    console.log(idArr)
     let tblBody = document.createElement("tbody");
-    let counter = 1;
-  for (let i = 0; i < 4; i++) {
-   
-    let row = document.createElement("tr");
+    let row1 = document.createElement("tr");
+    for (let i = 0; i < 4; i++) {
 
-    for (let j = 0; j < 4; j++) {
-     
-      let cell = document.createElement("td");
-      let picButton = document.createElement("button");
-      picButton.setAttribute("id", counter);
-      cell.appendChild(picButton);
-      row.appendChild(cell);
+        let cell = document.createElement("td");
+        let picButton = document.createElement("button");
+        picButton.setAttribute("id", idArr[i]);
+        cell.appendChild(picButton);
+        row1.appendChild(cell);
+        tblBody.appendChild(row1);  
     }
-    tblBody.appendChild(row);
-  }
-  tbl.appendChild(tblBody);
+    let row2 = document.createElement("tr");
+    for (let i = 4; i < 8; i++) {
+
+        let cell = document.createElement("td");
+        let picButton = document.createElement("button");
+        picButton.setAttribute("id", idArr[i]);
+        cell.appendChild(picButton);
+        row2.appendChild(cell);
+        tblBody.appendChild(row2);  
+    }
+    let row3 = document.createElement("tr");
+    for (let i = 8; i < 12; i++) {
+
+        let cell = document.createElement("td");
+        let picButton = document.createElement("button");
+        picButton.setAttribute("id", idArr[i]);
+        cell.appendChild(picButton);
+        row3.appendChild(cell);
+        tblBody.appendChild(row3);  
+    }
+    let row4 = document.createElement("tr");
+    for (let i = 12; i < 16; i++) {
+
+        let cell = document.createElement("td");
+        let picButton = document.createElement("button");
+        picButton.setAttribute("id", idArr[i]);
+        cell.appendChild(picButton);
+        row4.appendChild(cell);
+        tblBody.appendChild(row4);  
+    }
+    tbl.appendChild(tblBody);
 }
 
-function gameStart(){
+function gameStart() {
     countdown();
     setTimeout(endGame, 10000)
 }
-function endGame(){
-tbl.style.visibility = "hidden";
+
+function endGame() {
+    tbl.style.visibility = "hidden";
 }
 
-(function init(){
-    
+
+function countdown() {
+    let timeleft = 10;
+    let downloadTimer = setInterval(function() {
+        document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
+        timeleft -= 1;
+        if (timeleft <= 0) {
+            clearInterval(downloadTimer);
+            document.getElementById("countdown").innerHTML = "You ran out of time. Try again!"
+        }
+    }, 1000);
+}
+
+(function init() {
     container.style.visibility = "hidden";
     listeners();
     lvlOptions();
 })();
-function countdown(){
-    var timeleft = 10;
-  var downloadTimer = setInterval(function(){
-  document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
-  timeleft -= 1;
-  if(timeleft <= 0){
-    clearInterval(downloadTimer);
-    document.getElementById("countdown").innerHTML = "You ran out of time. Try again!"
-  }
-}, 1000);
-}
+
+function shuffle() {
+    let array = [0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7];
+	var currentIndex = array.length;
+	var temporaryValue, randomIndex;
+
+	// While there remain elements to shuffle...
+	while (0 !== currentIndex) {
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+
+		// And swap it with the current element.
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+
+	return array;
+
+};
