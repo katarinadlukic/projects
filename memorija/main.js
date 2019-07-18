4bfdd1570b0ac9a3408139d148944c58a6d4d4ca
katarinadlukic <40492319+katarinadlukic@users.noreply.github.com>
2019-07-18 07:04:57 +0200

    tabela, timer , countdown

--- memorija/main.js
+++ memorija/main.js
@@ -1,33 +1,33 @@
 let tryIt = document.getElementById("tryIt");
+let btnStart = document.getElementById("start");
 let container = document.getElementById("container");
 let scoreBoard = document.getElementById("scoreBoard");
 let gameDiv = document.getElementById("gameDiv");
+let tbl = document.getElementById("table"); 
     function enterGame() {
-            let x = readCookie("username");
-            if(x){
-                createElements(x);
-            }else {
-                let person = prompt("Please enter your name");
-                createCookie("username",person, 1);
-            }
-            
+            // let x = readCookie("username");
+            // if(x){
+            //     createElements(x);
+            // }else {
+            //     let person = prompt("Please enter your name");
+            //     createCookie("username",person, 1);
+            // }
+            let x = "Katarina"
+            createElements(x);
   }
   function listeners(){
-      
-      tryIt.addEventListener("click", enterGame)
+      tryIt.addEventListener("click", enterGame);
+       start.addEventListener("click", gameStart);
   }
-  (function init(){
-      container.style.visibility = "hidden";
-      listeners();
-  })();
+ 
 
   function createElements(person){
-    tryIt.style.display = "none"; // brisanje tryIt dugmeta sa strancie
+    tryIt.style.display = "none"; // brisanje tryIt dugmeta sa stranice
     container.style.visibility = "visible";
     let welcomeMsg = document.getElementById("welcome");
-            welcomeMsg.textContent = `Welcome ${person}!`;
-    
-
+     welcomeMsg.textContent = `Welcome ${person}!`;
+     generateTable()
+ 
   }
 
  
@@ -54,4 +54,68 @@ function readCookie(name) {
 
 function eraseCookie(name) {
 	createCookie(name,"",-1)
+}
+
+function lvlOptions(){
+    let lvlTxt = document.getElementById("lvlTxt");
+    lvlTxt.textContent = " You have 60 sec to finish the game";
+    let a = document.getElementById('level');
+    a.addEventListener('change', function() {
+       if (this.value == 0){
+        lvlTxt.textContent = " You have 60 sec to finish the game";
+    }
+        if (this.value == 1){
+            lvlTxt.textContent = " You have 30 sec to finish the game";
+        }
+        if (this.value == 2){
+            lvlTxt.textContent = " You have 15 sec to finish the game";
+        }
+    })
+}
+
+function generateTable(){
+    
+    let tblBody = document.createElement("tbody");
+    let counter = 1;
+  for (let i = 0; i < 4; i++) {
+   
+    let row = document.createElement("tr");
+
+    for (let j = 0; j < 4; j++) {
+     
+      let cell = document.createElement("td");
+      let picButton = document.createElement("button");
+      picButton.setAttribute("id", counter);
+      cell.appendChild(picButton);
+      row.appendChild(cell);
+    }
+    tblBody.appendChild(row);
+  }
+  tbl.appendChild(tblBody);
+}
+
+function gameStart(){
+    countdown();
+    setTimeout(endGame, 10000)
+}
+function endGame(){
+tbl.style.visibility = "hidden";
+}
+
+(function init(){
+    
+    container.style.visibility = "hidden";
+    listeners();
+    lvlOptions();
+})();
+function countdown(){
+    var timeleft = 10;
+  var downloadTimer = setInterval(function(){
+  document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
+  timeleft -= 1;
+  if(timeleft <= 0){
+    clearInterval(downloadTimer);
+    document.getElementById("countdown").innerHTML = "You ran out of time. Try again!"
+  }
+}, 1000);
 }
