1347abd32094790079f52c2a0ae4fbcbf875ccdc
katarinadlukic <40492319+katarinadlukic@users.noreply.github.com>
2019-07-21 19:01:51 +0200

    generate table, onclick, setTimeout

--- memorija/main.js
+++ memorija/main.js
@@ -1,27 +1,52 @@
-let xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://api.myjson.com/bins/gsx6p', true);
-xhr.onload = function() {
-    data = JSON.parse(xhr.responseText);
-}
-xhr.send();
-
+let data = [
+    {
+      "id": "0",
+      "url": "http://pngimg.com/uploads/mario/mario_PNG79.png"
+    },
+    {
+      "id": "1",
+      "url": "http://pngimg.com/uploads/mario/mario_PNG128.png"
+    },
+    {
+      "id": "2",
+      "url": "http://pngimg.com/uploads/mario/mario_PNG119.png"
+    },
+    {
+      "id": "3",
+      "url": "http://pngimg.com/uploads/mario/mario_PNG50.png"
+    },
+    {
+      "id": "4",
+      "url": "http://pngimg.com/uploads/mario/mario_PNG39.png"
+    },
+    {
+      "id": "5",
+      "url": "http://pngimg.com/uploads/mario/mario_PNG33.png"
+    },
+    {
+      "id": "6",
+      "url": "http://pngimg.com/uploads/mario/mario_PNG91.png"
+    },
+    {
+      "id": "7",
+      "url": "http://pngimg.com/uploads/mario/mario_PNG32.png"
+    }
+  ]
+  
 let tryIt = document.getElementById("tryIt");
 let btnStart = document.getElementById("start");
 let container = document.getElementById("container");
 let scoreBoard = document.getElementById("scoreBoard");
 let gameDiv = document.getElementById("gameDiv");
 let tbl = document.getElementById("table");
-  
-
+let imgId = [];
+let cellId  = [];
+let openElements = 0;
 (function init() {
     container.style.visibility = "hidden";
     listeners();
     lvlOptions();
-
 })();
-let target = [];
-
-
 function enterGame() {
     let x = readCookie("username");
     if(x){
@@ -30,26 +55,19 @@ function enterGame() {
         let person = prompt("Please enter your name");
         createCookie("username",person, 1);
     }
-    
     createElements(x);
 }
-
 function listeners() {
     tryIt.addEventListener("click", enterGame);
     start.addEventListener("click", gameStart);
 }
-
-
 function createElements(person) {
     tryIt.style.display = "none"; // brisanje tryIt dugmeta sa stranice
     container.style.visibility = "visible";
     let welcomeMsg = document.getElementById("welcome");
     welcomeMsg.textContent = `Welcome ${person}!`;
-    generateTable()
-
+    generateTable();
 }
-
-
 function createCookie(name, value, days) {
     if (days) {
         var date = new Date();
@@ -58,7 +76,6 @@ function createCookie(name, value, days) {
     } else var expires = "";
     document.cookie = name + "=" + value + expires + "; path=/";
 }
-
 function readCookie(name) {
     var nameEQ = name + "=";
     var ca = document.cookie.split(';');
@@ -69,11 +86,9 @@ function readCookie(name) {
     }
     return null;
 }
-
 function eraseCookie(name) {
     createCookie(name, "", -1)
 }
-
 function lvlOptions() {
     let lvlTxt = document.getElementById("lvlTxt");
     lvlTxt.textContent = " You have 60 sec to finish the game";
@@ -90,84 +105,59 @@ function lvlOptions() {
         }
     })
 }
-
 function generateTable() {
-    let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-    shuffle(arr); // [3,4,7,3,5,7,3,7,8,91,0] 
+    let picsId= [0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4, 5, 6, 7];
+    let arr = shuffle(picsId); // shuffled array
     let tblBody = document.createElement("tbody");
     let row1 = document.createElement("tr");
     for (let i = 0; i < 4; i++) {
-
-        let cell = document.createElement("td");
-        let picButton = document.createElement("button");
-        cell.setAttribute("id", arr[i]);
-        cell.addEventListener("click", check);
-        cell.appendChild(picButton);
-        row1.appendChild(cell);
-        tblBody.appendChild(row1);
+    row1.innerHTML += `<td id='${i}' onclick = "check(this,${arr[i]})" ></td>`;
+    tblBody.appendChild(row1);
     }
     let row2 = document.createElement("tr");
-    for (let i = 4; i < 8; i++) {
-
-        let cell = document.createElement("td");
-        let picButton = document.createElement("button");
-        cell.setAttribute("id", arr[i]);
-        cell.addEventListener("click", check);
-        cell.appendChild(picButton);
-        row2.appendChild(cell);
-        tblBody.appendChild(row2);
+    for (let i = 4; i < 8; i++) {   
+     row2.innerHTML += `<td id='${i}' onclick ="check(this,${arr[i]})"></td>`;
+     tblBody.appendChild(row2);
     }
     let row3 = document.createElement("tr");
-    for (let i = 8; i < 12; i++) {
-
-        let cell = document.createElement("td");
-        let picButton = document.createElement("button");
-        cell.setAttribute("id", arr[i]);
-        cell.addEventListener("click", check);
-        cell.appendChild(picButton);
-        row3.appendChild(cell);
-        tblBody.appendChild(row3);
+    for (let i = 8; i < 12; i++) {     
+    row3.innerHTML += `<td id='${i}' onclick ="check(this,${arr[i]})"></td>`;
+    tblBody.appendChild(row3);
     }
     let row4 = document.createElement("tr");
     for (let i = 12; i < 16; i++) {
-
-        let cell = document.createElement("td");
-        let picButton = document.createElement("button");
-        cell.setAttribute("id", arr[i]);
-        cell.addEventListener("click", check);
-        cell.appendChild(picButton);
-        row4.appendChild(cell);
-        tblBody.appendChild(row4);
+     row4.innerHTML += `<td id='${i}' onclick ="check(this,${arr[i]})"></td>`;
+     tblBody.appendChild(row4);
     }
     tbl.appendChild(tblBody);
+    console.log(arr);
 }
-
-function gameStart() {
-
-    countdown();
-    setTimeout(endGame, 10000)
-}
-let arrX = [0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7];
-shuffle(arrX);
-console.log(arrX)
-
-
-function check() {
-
-    let x = this;
-    let counter = 0;
-    for (let i = 0; i < arrX.length; i++) {
-
-        x.innerHTML = `<img id="${arrX[i]}" src="${data[arrX[i]].url}">`;
-
+function check(element,value) {
+    let imageToCompare = data[value].url;
+    if(element.innerHTML == "" && imgId.length < 2){
+        element.innerHTML = `<img id=${value} src='${imageToCompare}'/>`;
+        if(imgId.length == 0){
+            imgId.push(value);
+            cellId.push(element.id);
+        }else if(imgId.length == 1){
+            imgId.push(value);
+            cellId.push(element.id);
+            if(imgId[0] == imgId[1]){
+                openElements += 2;
+                imgId = [];
+                cellId = [];
+            }else {
+                function removePictures(){
+                    document.getElementById(cellId[1]).innerHTML = "";
+                    document.getElementById(cellId[0]).innerHTML = "";
+                    imgId = [];
+                    cellId = [];
+                }
+                setTimeout(removePictures,600);
+            }
+        }
     }
-    console.log(x.id, x.children[0].id)
 }
-
-function endGame() {
-    tbl.style.visibility = "hidden";
-}
-
 function countdown() {
     let timeleft = 10;
     let downloadTimer = setInterval(function() {
@@ -179,25 +169,15 @@ function countdown() {
         }
     }, 1000);
 }
-
-
-
-function shuffle(array) {
+function shuffle(array) { 
     var currentIndex = array.length;
     var temporaryValue, randomIndex;
-
-    // While there remain elements to shuffle...
     while (0 !== currentIndex) {
-        // Pick a remaining element...
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
-
-        // And swap it with the current element.
         temporaryValue = array[currentIndex];
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
     }
-
     return array;
-
 };
