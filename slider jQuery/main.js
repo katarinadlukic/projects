0abbadb6ce7d9265a5213355b3e5c805ee8d479d
katarinadlukic <katarinadlukic@gmail.com>
2019-08-01 10:31:42 +0200

    komentari

--- slider jQuery/main.js
+++ slider jQuery/main.js
@@ -1,66 +1,79 @@
-function getJson(url) {
-  return JSON.parse($.ajax({
-      type: 'GET',
-      url: url,
-      dataType: 'json',
-      global: false,
-      async: false,
-      success: function (data) {
-          return data;
+    let data = getJson('https://api.myjson.com/bins/1as1kd');
+    let intervalId =  setInterval(interval, 2000);
+    let color = "gray"; // boja pozadine i bordera
+    let time = 3000; // tajmer za autoplay
+    let showText = true; // sa ili bez texta
+    let i = 0;
+    function getJson(url) { // ajax poziv
+      return JSON.parse($.ajax({
+          type: 'GET',
+          url: url,
+          dataType: 'json',
+          global: false,
+          async: false,
+          success: function (data) {
+              return data;
+          }
+      }).responseText);
+    }
+
+    function init(color){ // onload
+        $("#main_image").html(( `<img src="slike/${data[0].big_image}"/>`)) ;
+        $("#image_box").css('height', "300px");
+        if (showText){
+          imageText(0);
+          $("#image_box").css('height', "370px");
+          $("#image_box").css('background-color', color);
+          $("#0").css('background-color', color);
+        }
+        $.each(data, function(index, element) {
+          $("#" + index).html(( `<img src="slike/${element.thumb_image}"/>`)) ;
+        });
+    }
+   
+    $("#thumb_table").click(function(event){  // klik na thumb sliku
+      let x = event.target.parentNode.id;
+      displayImages(x, color);
+    });
+ 
+    $("#right_arrow").click(function(event){   // klik na desnu strelicu
+      clearInterval(intervalId);
+      i++;
+      if(i == 9){i = 0;}
+        displayImages(i, color);
+        intervalId =  setInterval(interval,time);
+    });
+   
+    $("#left_arrow").click(function(event){  //klik na levu strelicu
+      clearInterval(intervalId);
+      i--;
+      if(i < 0){i = 8;}
+        displayImages(i, color);
+        intervalId = setInterval(interval, time);
+    });
+    
+    function displayImages(x,color){ // funkcija za izbacivanje slika
+      $("#thumb_table").find("div").css('background-color', 'white');
+      $("#main_image").hide().html(( `<img src="slike/${data[x].big_image}"/>`)).fadeIn(500);
+      $("#image_box").css('height', "300px");
+      if (showText){
+        imageText(x);
+        $("#image_box").css('height', "370px");
+        $("#image_box").css('background-color', color);
+        $("#" + x).css('background-color', color);
       }
-  }).responseText);
-}
-let data = getJson('https://api.myjson.com/bins/1as1kd');
-let intervalId =  setInterval(interval, 4000);
-function init(){
-  $("#main_image").html(( `<img src="${data[0].big_image}"/>`)) ;
-  $("#0").css('background-color', 'rgb(27, 26, 26)');
-  $("#title").html(data[0].title);
-  $("#description").html(data[0].description);
-  $.each(data, function(index, element) {
-   $("#" + index).html(( `<img src="${element.thumb_image}"/>`)) ;
- });
-}
-// klik na thumb sliku
-$("#thumb_table").click(function(event){
-  let x = event.target.parentNode.id;
-  displayImages(x);
-});
-let i = 0;
-// klik na desnu strelicu
-$("#right_arrow").click(function(event){
-  clearInterval(intervalId);
-  i++;
-  if(i == 9){
-    i = 0;
-  }
-    displayImages(i);
-    intervalId =  setInterval(interval, 4000);
-});
-//klik na levu strelicu
-$("#left_arrow").click(function(event){
-  clearInterval(intervalId);
-  i--;
-  if(i < 0){
-    i = 8;
-  }
-    displayImages(i);
-    intervalId = setInterval(interval, 4000);
-});
-// funkcija za izbacivanje slika
-function displayImages(x){
-  $("#thumb_table").find("div").css('background-color', 'white');
-  $("#main_image").hide().html(( `<img src="${data[x].big_image}"/>`)).fadeIn(500);
-  $("#title").html(data[x].title);
-  $("#description").html(data[x].description);
-  $("#" + x).css('background-color', 'rgb(27, 26, 26)');
-}
-function interval(){ 
-  i++;
-  if(i == 9){
-    i = 0;
-  }
-  displayImages(i);
- };
- init();
+    }
+   
+    function interval(){  // funkcija za autoplay
+      i++;
+      if(i == 9){ i = 0; }
+      displayImages(i, color);
+     };
+  
+     function imageText(x){ // prikaz texta ispod slike
+      $("#title").html(data[x].title);
+      $("#description").html(data[x].description);
+     }
+     init(color);
+
 
